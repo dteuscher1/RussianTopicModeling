f555cfc4a53331d06e1dde471c8133e9f0cb2214
David Teuscher <44874961+dteuscher1@users.noreply.github.com>
2022-03-30 22:38:33 -0600

    Comment code and begin getting topic weights

--- Project.py
+++ Project.py
@@ -1,13 +1,24 @@
+"""
+Author: David Teuscher
+Last Edited: 30.03.22
+This script takes scrapes the text for songs by Viktor Tsoy and
+writes them to a .txt file
+"""
+
+# Load need packages
 from bs4 import BeautifulSoup as bs
 import requests
 import justext
 import urllib.parse
 
+# URLs to scrape links from
 urls = ["https://lyric-text.ru/viktor-tsoy/", "https://lyric-text.ru/viktor-tsoy/page-2/",
         "https://lyric-text.ru/viktor-tsoy/page-3/", "https://lyric-text.ru/viktor-tsoy/page-4/"]
 
+# Create empty list for links and song title
 links = []
 song_title = []
+# Loop through the four URLs to scrape from
 for url in urls:
     # Specify header information related to your computer
     headers = {
@@ -16,18 +27,23 @@ for url in urls:
     response = requests.get(url, headers=headers)
     # Parse the HTML content on the page
     soup = bs(response.content, "html.parser")
+    # Find all the div tags with the specific class
     paragraphs = soup.find_all("div", {'class': 'content_box'})
+    # Loop through each found div tag
     for paragraph in paragraphs:
+        # Pull all the anchors out
         anchors = paragraph.find_all('a')
+        # Try to take the href from the anchor tag if possible
         for anchor in anchors:
             try:
                 links.append(anchor.attrs['href'])
                 song_title.append(anchor.get_text())
+            # print that the anchor is skipped if there is a KeyError resulting from no href attribute
             except KeyError:
                 print("skipping this anchor because it doesn't have an href attribute")
 
 
-
+# Create function to create the absolute path based on the relative link
 def get_absolute_path_link(url, relative_link):
     parsed_url = urllib.parse.urlparse(url)
     return urllib.parse.urljoin(parsed_url.scheme + "://" + parsed_url.netloc, relative_link)
@@ -35,17 +51,19 @@ def get_absolute_path_link(url, relative_link):
 # Get the absolute path for each link
 links = [get_absolute_path_link(url, l) for l in links]
 
-print(links)
-print(song_title)
-current_url = links[0]
-
+# Open an output file for the songs
 with open(f"songs.txt", mode='w') as outfile:
+    # Write a header for the file
     outfile.write("Title" + "\t" + " Lyrics" + '\n')
+    # Loop through the links
     for i in range(len(links)):
+        # Get the HTML content
         response = requests.get(links[i], headers=headers)
         # Parse the HTML content on the page
         soup = bs(response.content, "html.parser")
+        # Find all the divs with the specific class and then the paragraph inside
         paragraphs = soup.find('div', {'class': 'content_box'}).find('p')
+        # Write out to text file if there is text
         try:
             outfile.write(song_title[i] + "\t" + paragraphs.get_text(strip=True) + '\n')
             print(f'Writing {song_title[i]}')
--- Work.py
+++ Work.py
@@ -1,3 +1,10 @@
+"""
+Author: David Teuscher
+Last Edited: 30.03.22
+This script takes news articles and performs topic modeling for the articles.
+The top 10 words for the topics are written out to a .csv file.
+"""
+
 import pandas as pd
 import os
 import re
@@ -36,24 +43,24 @@ def preprocess_text(text):
     text = " ".join(tokens).replace(",", " ").replace("«", " ").replace("–", " ").replace("--", " ").replace("—", " ").replace("-", " ").replace("»", " ").replace("\"", " ").replace("::", " ")
 
     return text
-
-filenames = [i for i in os.listdir() if re.search(r"\.txt", i)]
-# Make the text of all the files a string
-doc_complete = [file2str(i) for i in filenames]
-
-books_complete = [preprocess_text(i) for i in doc_complete]
-books_complete = [book.split() for book in books_complete]
-corpus = corpora.Dictionary(books_complete)
-
-doc_term_matrix = [corpus.doc2bow(book) for book in books_complete]
-
-os.chdir("../")
-Lda = gensim.models.ldamodel.LdaModel
-# Fit with 4 topics
-lda_model = Lda(doc_term_matrix, num_topics=4, id2word=corpus, passes=50)
-# Visualize the topics and save as HTML file
-visualization = gensim_models.prepare(lda_model, doc_term_matrix, corpus)
-save_html(visualization, "visualization_books.html")
+#
+# filenames = [i for i in os.listdir() if re.search(r"\.txt", i)]
+# # Make the text of all the files a string
+# doc_complete = [file2str(i) for i in filenames]
+#
+# books_complete = [preprocess_text(i) for i in doc_complete]
+# books_complete = [book.split() for book in books_complete]
+# corpus = corpora.Dictionary(books_complete)
+#
+# doc_term_matrix = [corpus.doc2bow(book) for book in books_complete]
+#
+# os.chdir("../")
+# Lda = gensim.models.ldamodel.LdaModel
+# # Fit with 4 topics
+# lda_model = Lda(doc_term_matrix, num_topics=4, id2word=corpus, passes=50)
+# # Visualize the topics and save as HTML file
+# visualization = gensim_models.prepare(lda_model, doc_term_matrix, corpus)
+# save_html(visualization, "visualization_books.html")
 # Combine all lyrics together
 song_lyrics = []
 for song in songs[' Lyrics']:
@@ -75,3 +82,15 @@ visualization = gensim_models.prepare(lda_model, doc_term_matrix, corpus)
 save_html(visualization, "visualization_songs.html")
 
 # Extract the top words for each topic
+#setup data-frame
+weights_output = pd.DataFrame(columns = ['topic', 'prob_weight', 'doc_id'])
+
+#extraction loop
+for i in range(0, len(doc_term_matrix)):
+    doc_weights = lda_model[doc_term_matrix[i]]
+    weights_df = pd.DataFrame(doc_weights, columns = ['topic', 'prob_weight'])
+    weights_df['doc_id'] = i
+    weights_output = weights_output.append(weights_df)
+
+#save information in the format you use
+weights_output.to_csv("topic_weights_bydoc.csv")
